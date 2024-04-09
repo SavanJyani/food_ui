9543ef8b9b2028ca58a7a5c3f4c7787868f21dc2
SavanJyani <patelsavan7925@gmail.com>
2024-04-09 12:48:55 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,6 @@ class MyApp extends StatelessWidget {
       navigatorKey: StackedService.navigatorKey,
       onGenerateRoute: StackedRouter().onGenerateRoute,
       initialRoute: "/",
-      //home: SearchItemScreenView(),
     );
   }
 }
